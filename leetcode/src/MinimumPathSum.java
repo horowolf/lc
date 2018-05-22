b110051545abefac57d95f91200daf35d4150a3a
horowolf <lucky801991@gmail.com>
2018-05-22 12:26:16 -0400

    Q64

--- leetcode/src/MinimumPathSum.java
+++ leetcode/src/MinimumPathSum.java
@@ -5,42 +5,40 @@ public class MinimumPathSum {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[][] input = {{1,3,1}};
+		int[][] input = {{1,3,1},
+		                 {1,5,1},
+		                 {4,2,1}};
 		System.out.println(String.valueOf(minPathSum(input)));
 	}
 	public static int minPathSum(int[][] grid) {
         int ans = -1;
-		
-		if (grid.length < 1 && grid[0].length < 1) {
-			return grid[0][0];
-		}
-		
-		if (!(grid.length < 1)) {
-			for (int i = 1; i < grid[0].length ; i++) {
-            	grid[i][0] = grid[i - 1][0] + grid[i][0];
-            }
-			
-		}
-		
-        if (!(grid[0].length < 1)) {
-        	for (int i = 1; i < grid.length ; i++) {
-	        	grid[0][i] = grid[0][i - 1] + grid[0][i];
-	        }
+        // check if grid is empty
+        if (grid.length == 0) {
+        	return 0;
         }
-        
-        
-        if (grid.length < 1 || grid[0].length < 1) {
-			return grid[grid[0].length - 1][grid.length - 1];
-		}
-        
-        for (int i = 1; i < grid.length ; i++) {
-        	for (int j = 1; j < grid[0].length ; j++) {
-        		grid[j][i] = grid[j][i] + min(grid[j - 1][i], grid[j][i - 1]);
+        //start from top-left, skip (0,0)
+        int x = 1;
+        int y = 0;
+        int a = 0; // left
+        int b = 0; // up
+        for (; y < grid.length; y++) { // go through every elements
+        	for (; x < grid[y].length; x++) {
+        		if (x == 0) {
+        			a = Integer.MAX_VALUE;
+        			b = grid[y - 1][x];
+        		} else if (y == 0) {
+        			a = grid[y][x - 1];
+        			b = Integer.MAX_VALUE;
+        		} else {
+        			a = grid[y][x - 1];
+        			b = grid[y - 1][x];
+        		}
+        		// x = 0, y = 0 should never happen
+        		grid[y][x] = grid[y][x] + min(a, b);
         	}
+        	x = 0;
         }
-        
-        
-        return grid[grid[0].length - 1][grid.length - 1];
+        return grid[grid.length - 1][grid[0].length - 1];
     }
 	
 	private static int min(int a, int b) {
