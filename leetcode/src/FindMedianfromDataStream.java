573300001af724e28ff2175d3a78dc9ee1348c84
horowolf <lucky801991@gmail.com>
2018-06-01 14:29:23 -0400

    Q 295 bug needs to be fixed

--- leetcode/src/FindMedianfromDataStream.java
+++ leetcode/src/FindMedianfromDataStream.java
@@ -4,27 +4,128 @@ public class FindMedianfromDataStream {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		MedianFinder m = new MedianFinder();
+		m.addNum(6);
+		System.out.println(m.findMedian());
+		m.addNum(10);
+		System.out.println(m.findMedian());
+		m.addNum(2);
+		System.out.println(m.findMedian());
+		m.addNum(6);
+		System.out.println(m.findMedian());
+		m.addNum(5);
+		System.out.println(m.findMedian());
+		m.addNum(0);
+		System.out.println(m.findMedian()); //X
+		m.addNum(6);
+		System.out.println(m.findMedian());
+		m.addNum(3);
+		System.out.println(m.findMedian()); //X
+		m.addNum(1);
+		System.out.println(m.findMedian());
+		m.addNum(0);
+		System.out.println(m.findMedian());
+		m.addNum(0);
+		System.out.println(m.findMedian());
 	}
 
 }
 
 class MedianFinder { // Q 295
+	Node root = null;
 
     /** initialize your data structure here. */
     public MedianFinder() {
     }
     
     public void addNum(int num) {
+    	if (root == null) {
+    		root = new Node(num);
+    	} else {
+    		adder(root, num);
+    	}
+    }
+    
+    public void adder(Node n, int num) {
+    	if (n.val == num) {
+    		n.numThis++;
+    		return;
+    	}
+    	if (num < n.val) {
+    		n.numL++;
+    		if (n.left != null) {
+    			adder(n.left, num);
+    		} else {
+    			n.left = new Node(num);
+    		}
+    	} else {
+    		n.numR++;
+    		if (n.right != null) {
+    			adder(n.right, num);
+    		} else {
+    			n.right = new Node(num);
+    		}
+    	}
     }
     
     public double findMedian() {
+    	int total = root.numL + root.numThis + root.numR;
+    	int extra = (total + 1) % 2;
+    	int mid = (total + 1) / 2; // mid of 0 ~ n-1
     	
-    	return 0;
+    	return finder(root, mid, extra);
+    }
+    
+    public double finder(Node n, int kth, int extra) {
+    	if (extra == 0) {
+    		if (n.numL >= kth) {
+    			return finder(n.left, kth, 0);
+    		} else if (n.numL + n.numThis >= kth) {
+    			return n.val;
+    		} else {
+    			return finder(n.right, kth - (n.numL + n.numThis), 0);
+    		}
+    	} else {
+    		if (n.numL >= kth + 1) { // both left
+    			return finder(n.left, kth, 1);
+    		} else if (n.numL + n.numThis >= kth + 1 && n.numThis > 1) { // both this
+    			return n.val;
+    		} else if (n.numL + n.numThis < kth){ // both right
+    			return finder(n.right, kth - (n.numL + n.numThis), 1);
+    		} else {
+    			double a = 0;
+    			double b = 0;
+    			if (n.numL >= kth) {
+        			a = finder(n.left, kth, 0);
+        		} else if (n.numL + n.numThis >= kth) {
+        			a = n.val;
+        		} else {
+        			a = finder(n.right, kth - (n.numL + n.numThis), 0);
+        		}
+    			if (n.numL >= kth + 1) {
+        			b = finder(n.left, kth + 1, 0);
+        		} else if (n.numL + n.numThis >= kth + 1) {
+        			b = n.val;
+        		} else {
+        			b = finder(n.right, kth + 1 - (n.numL + n.numThis), 0);
+        		}
+    			return (a + b) / 2.0;
+    		}
+    	}
     	
     }
     
     class Node {
+    	int val;
+    	int numThis = 1;
+    	Node left = null;
+    	int numL = 0;
+    	Node right = null;
+    	int numR = 0;
+    	
+    	protected Node (int value) {
+    		val = value;
+    	}
     	
     }
 }
