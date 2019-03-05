c34757d16f3c327b99871ef9b9c2f9fb2bbdc447
horo <horo@192.168.5.219>
2019-03-04 20:20:35 -0500

    342 with bitCount

--- leetcode/src/PowerofFour.java
+++ leetcode/src/PowerofFour.java
@@ -3,10 +3,10 @@ public class PowerofFour {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(isPowerOfFour(5));
+		System.out.println(isPowerOfFour(4));
 	}
 	public static boolean isPowerOfFour(int num) {
-        return num > 0 && ((num & (num - 1)) == 0) && (num & 0xaaaaaaaa) == 0;
+        return num > 0 && (Integer.bitCount(num) == 1) && (num & 0xaaaaaaaa) == 0;
     }
 
 }
