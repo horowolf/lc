a92a617f2001994431299f32d828ede5cdcd6d4d
horowolf <lucky801991@gmail.com>
2019-04-27 22:51:41 -0400

    250 fix

--- leetcode/src/CountUnivalueSubtrees.java
+++ leetcode/src/CountUnivalueSubtrees.java
@@ -1,39 +1,59 @@
 
 public class CountUnivalueSubtrees {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TreeNode root = new TreeNode(5);
+		TreeNode l1 = new TreeNode(1);
+		TreeNode r1 = new TreeNode(5);
+		l1.left = new TreeNode(5);
+		l1.right = new TreeNode(5);
+		r1.right = new TreeNode(5);
+		root.left = l1;
+		root.right = r1;
+		
+		System.out.println(countUnivalueSubtrees(root));
+	}
+	
+	
 	static int count = 0;
-	public int countUnivalueSubtrees(TreeNode root) {
+	public static int countUnivalueSubtrees(TreeNode root) {
 		check(root);
 		return count;
 	}
 	
-	private boolean check(TreeNode subtree) {
+	private static boolean check(TreeNode subtree) {
 		if (subtree.left == null && subtree.right == null) {
 			count ++;
 			return true;
 		}
+		boolean flag = true;
 		if (subtree.left != null) {
 			if (check(subtree.left)) {
 				if (subtree.val != subtree.left.val) {
-					return false;
+					flag = false;
 				}
 			} else {
-				return false;
+				flag = false;
 			}
 		}
 		if (subtree.right != null) {
 			if (check(subtree.right)) {
 				if (subtree.val != subtree.right.val) {
-					return false;
+					flag = false;
 				}
 			} else {
-				return false;
+				flag = false;
 			}
+		}
+		if (flag == false) {
+			return false;
 		}
 		count ++;
 		return true;
 	}
 	
-	private class TreeNode {
+	private static class TreeNode {
 		int val;
 		TreeNode left;
 		TreeNode right;
