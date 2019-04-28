1e49bdb210e99461444b96fae305493994f0d883
horowolf <lucky801991@gmail.com>
2019-04-28 13:04:15 -0400

    122 (Add to 121 code)

--- leetcode/src/BestTimeBuyandSellStockEasy.java
+++ leetcode/src/BestTimeBuyandSellStockEasy.java
@@ -1,6 +1,6 @@
 
 public class BestTimeBuyandSellStockEasy {
-	public int maxProfit(int[] prices) {
+	public int maxProfit(int[] prices) { // 121
 		if (prices.length < 2) {
 			return 0;
 		}
@@ -14,6 +14,28 @@ public class BestTimeBuyandSellStockEasy {
         	}
         	
         }
+        
+		return result;
+    }
+	
+	public int totalMaxProfit(int[] prices) { // 122
+		if (prices.length < 2) {
+			return 0;
+		}
+        int result = 0; // current max profit
+        int profit = 0;
+        int buyPrice = prices[0];
+        for (int i = 1; i < prices.length; i++) {
+        	if (prices[i - 1] > prices[i]) {
+        		buyPrice = prices[i];
+        		result += profit;
+        		profit = 0;
+        	} else if (prices[i] - buyPrice > profit) {
+        		profit = prices[i] - buyPrice;
+        	}
+        	
+        }
+        result += profit;
         
 		return result;
     }
