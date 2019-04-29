0f09fd909de8575c828641476e994bc8f55b45c4
horowolf <lucky801991@gmail.com>
2019-04-29 18:58:04 -0400

    162 add comment

--- leetcode/src/FindPeakElement.java
+++ leetcode/src/FindPeakElement.java
@@ -1,9 +1,10 @@
 
-public class FindPeakElement {
+public class FindPeakElement { // to find a peek => just find this graphic / \
     public int findPeakElement(int[] nums) {
         if (nums.length == 1) {
         	return 0;
         }
+        // check if edge is answer
         if (nums[0] > nums[1]) {
         	return 0;
         } else if (nums[nums.length - 1] > nums[nums.length - 2]) {
@@ -11,13 +12,14 @@ public class FindPeakElement {
         }
         int left = 0;
         int right = nums.length - 1;
+        // edge is not answer => the element next to edge will increase
         
         while (true) {
         	int mid = (left + right) / 2;
-        	if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) {
+        	if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) { // hit the target
         		return mid;
         	}
-        	if (nums[mid] < nums[mid - 1]) {
+        	if (nums[mid] < nums[mid - 1]) { // at least one side of mid is increasing
         		right = mid;
         	} else {
         		left = mid;
