a1c76f189ffaf18ba484d93f2a4b8d7c6ad0c1a1
horowolf <lucky801991@gmail.com>
2019-05-03 18:38:40 -0400

    188 add into 123. need to review

--- leetcode/src/BestTimetoBuyandSellStock.java
+++ leetcode/src/BestTimetoBuyandSellStock.java
@@ -13,13 +13,37 @@ public class BestTimetoBuyandSellStock {
 	public static int maxProfit(int[] prices) { // Q123
 		int buy1 = Integer.MAX_VALUE, buy2 = Integer.MAX_VALUE;
         int sell1 = 0, sell2 = 0;
-        for(int i:prices){
+        for (int i:prices) {
             sell2 = Math.max(sell2, i - buy2);
             buy2    = Math.min(buy2,    i - sell1);
             sell1 = Math.max(sell1, i - buy1);
             buy1    = Math.min(buy1,    i);
         }
         return sell2;
+    }
+	
+	public int maxProfit(int k, int[] prices) { // Q188 
+		int len = prices.length;
+        if (k >= len / 2) return quickSolve(prices);
+        
+        int[][] t = new int[k + 1][len];
+        for (int i = 1; i <= k; i++) {
+            int tmpMax =  -prices[0];
+            for (int j = 1; j < len; j++) {
+                t[i][j] = Math.max(t[i][j - 1], prices[j] + tmpMax);
+                tmpMax =  Math.max(tmpMax, t[i - 1][j - 1] - prices[j]);
+            }
+        }
+        return t[k][len - 1];
+    }
+	
+
+    private int quickSolve(int[] prices) {
+        int len = prices.length, profit = 0;
+        for (int i = 1; i < len; i++)
+            // as long as there is a price gap, we gain a profit.
+            if (prices[i] > prices[i - 1]) profit += prices[i] - prices[i - 1];
+        return profit;
     }
 }
 
@@ -38,5 +62,30 @@ original of this solution from discussion:
         }
         return release2;
 
+
+*/
+
+
+/*  Memory Limit Exceeded
+
+		if (k == 0) {
+			return 0;
+		}
+        int[] buy = new int[k];
+        int[] sell = new int[k];
+        for (int i = 0; i < k; i++) {
+        	buy[i] = Integer.MAX_VALUE;
+        }
+		for (int i: prices) {
+			for (int j = k - 1; j > 0; j--) {
+				sell[j] = Math.max(sell[j], i - buy[j]);
+				buy[j] = Math.min(buy[j], i - sell[j - 1]);
+			}
+			sell[0] = Math.max(sell[0], i - buy[0]);
+			buy[0] = Math.min(buy[0], i);
+		}
+		
+        
+        return sell[k - 1];
 
 */
